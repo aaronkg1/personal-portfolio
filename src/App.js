663e483aa23137a0ed113cd7ddcbfb9115d08ef1
aaronkg1 <aaronkg1@gmail.com>
2022-07-25 20:04:11 +0100

    feat: Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
