bac1d8e3e8432aea6e7d9ddbc705e51d4d1a2c2e
aaronkg1 <aaronkg1@gmail.com>
2022-07-27 17:21:05 +0100

    docs: Updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import "./App.css";
+import "./styles/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Nav from "./components/Nav";
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
