e5fc48045ce2d1064c723779f9cda8d624a897a6
aaronkg1 <aaronkg1@gmail.com>
2022-07-28 13:30:31 +0100

    feat: Added text label indicating selection

--- src/components/SpinningMenu.js
+++ src/components/SpinningMenu.js
@@ -7,36 +7,80 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Spinner.css";
 import { Link } from "react-router-dom";
 
 const Spinner = () => {
   const [active, setActive] = useState(false);
+  const [currentHover, setCurrentHover] = useState("");
+  const [display, setDisplay] = useState("");
 
   const toggleMenu = () => {
     setActive(!active);
   };
 
+  useEffect(() => {}, [currentHover]);
+
+  const toggleCurrentHover = (value) => {
+    setCurrentHover(value);
+  };
+
+  const unsetCurrentHoverValue = () => {
+    setCurrentHover("");
+  };
+
   return (
     <ul className={active ? `spinner-container active` : "spinner-container"}>
-      <li className="avatar-picture" onClick={toggleMenu}></li>
+      <li className="spinner-title" onClick={toggleMenu}>
+        Menu
+      </li>
+      <li className="avatar-picture" onClick={toggleMenu}>
+        <p className="menu-label">{currentHover}</p>
+      </li>
       <li className="spinner-item" style={{ "--i": 0 }}>
-        <Link to="/">
+        <Link
+          to="/"
+          onMouseEnter={() => {
+            toggleCurrentHover("Home");
+          }}
+          onMouseLeave={unsetCurrentHoverValue}
+        >
           <FontAwesomeIcon icon={faHouse} className="menu-icon" />
         </Link>
       </li>
-      <li className="spinner-item" style={{ "--i": 1 }}>
+      <li
+        className="spinner-item"
+        style={{ "--i": 1 }}
+        onMouseEnter={() => {
+          toggleCurrentHover("About");
+        }}
+        onMouseLeave={unsetCurrentHoverValue}
+      >
         <Link to="/about">
           <FontAwesomeIcon icon={faCircleInfo} className="menu-icon" />
         </Link>
       </li>
-      <li className="spinner-item" style={{ "--i": 2 }}>
+      <li
+        className="spinner-item"
+        style={{ "--i": 2 }}
+        onMouseEnter={() => {
+          toggleCurrentHover("Projects");
+        }}
+        onMouseLeave={unsetCurrentHoverValue}
+      >
         <Link to="/projects">
           <FontAwesomeIcon icon={faDiagramProject} className="menu-icon" />
         </Link>
       </li>
-      <li className="spinner-item" style={{ "--i": 3 }}>
+      <li
+        className="spinner-item"
+        style={{ "--i": 3 }}
+        onMouseEnter={() => {
+          toggleCurrentHover("LinkedIn");
+        }}
+        onMouseLeave={unsetCurrentHoverValue}
+      >
         <a
           href="https://www.linkedin.com/in/aaron-kenyon-gewirtz/"
           target="_blank"
@@ -45,7 +89,14 @@ const Spinner = () => {
           <FontAwesomeIcon icon={faLinkedin} className="menu-icon" />
         </a>
       </li>
-      <li className="spinner-item" style={{ "--i": 4 }}>
+      <li
+        className="spinner-item"
+        style={{ "--i": 4 }}
+        onMouseEnter={() => {
+          toggleCurrentHover("Github");
+        }}
+        onMouseLeave={unsetCurrentHoverValue}
+      >
         <a
           href="https://github.com/aaronkg1"
           target="_blank"
@@ -54,7 +105,14 @@ const Spinner = () => {
           <FontAwesomeIcon icon={faGithub} className="menu-icon" />
         </a>
       </li>
-      <li className="spinner-item" style={{ "--i": 5 }}>
+      <li
+        className="spinner-item"
+        style={{ "--i": 5 }}
+        onMouseEnter={() => {
+          toggleCurrentHover("Email");
+        }}
+        onMouseLeave={unsetCurrentHoverValue}
+      >
         <a href="mailto:aaronkg1@gmail.com">
           <FontAwesomeIcon icon={faEnvelope} className="menu-icon" />
         </a>
