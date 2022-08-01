83ddad37ba32164ad1f252f9b737f8e220ccfd05
aaronkg1 <aaronkg1@gmail.com>
2022-08-01 14:47:38 +0100

    feat: Updated project card component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,38 @@
 import React from "react";
-
+import "../styles/ProjectCard.css";
 const ProjectCard = (props) => {
-  const { title, description, url } = props;
+  const { title, description, url, position, liveLink, repository } = props;
 
   return (
-    <li key={`${title}ID`}>
-      <h2></h2>
+    <li key={`${title}ID`} className="project-card">
+      {position === "left" ? (
+        <div className="image-wrapper">
+          <img src={url} alt={title} className="hover-zoom" />
+        </div>
+      ) : null}
+      <div className="info-container">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div className="button-wrapper">
+          <button className="project-button">
+            <a href={liveLink} alt={title}>
+              Live App
+            </a>
+          </button>
+          <button className="project-button">
+            <a href={repository} alt={`${title} repository`}>
+              Repository
+            </a>
+          </button>
+        </div>
+      </div>
+      {position === "right" ? (
+        <div className="image-wrapper">
+          <img src={url} alt={title} className="hover-zoom" />
+        </div>
+      ) : null}
     </li>
   );
 };
+
+export default ProjectCard;
