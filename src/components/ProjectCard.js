113e83940b9a39e03a939d2d5d5c887e0c73b21e
aaronkg1 <aaronkg1@gmail.com>
2022-08-01 11:05:29 +0100

    feat: Created project card component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectCard = (props) => {
+  const { title, description, url } = props;
+
+  return (
+    <li key={`${title}ID`}>
+      <h2></h2>
+    </li>
+  );
+};
