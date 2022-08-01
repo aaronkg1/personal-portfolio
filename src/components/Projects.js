7dc13271da5c82350533cb4c198588cd05e4d610
aaronkg1 <aaronkg1@gmail.com>
2022-08-01 11:03:49 +0100

    feat: Created projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "../styles/Projects.css";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="projects-container" id="projects">
+      <h1 className="section-title">Projects</h1>
+      <ul className="project-grid">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+  );
 };
 
 export default Projects;
