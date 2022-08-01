4ec728786e1718d87ec8ca971f780c300e0c3f25
aaronkg1 <aaronkg1@gmail.com>
2022-08-01 14:49:25 +0100

    feat: Added information for project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,66 @@
 import React from "react";
 import "../styles/Projects.css";
+import wheresWally from "../assets/projects/where-is-wally.png";
+import battleships from "../assets/projects/battleships.png";
+import weather from "../assets/projects/weather.png";
+import etchasketch from "../assets/projects/etch-a-sketch.png";
+import cvcreator from "../assets/projects/cvproject.png";
+import socialmedia from "../assets/projects/social-media-clone.png";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
     <section className="projects-container" id="projects">
       <h1 className="section-title">Projects</h1>
       <ul className="project-grid">
-        <li></li>
-        <li></li>
-        <li></li>
+        <ProjectCard
+          title="Social Media Clone"
+          description="Social media app based on the Facebook design built with React, Redux and Firebase."
+          url={socialmedia}
+          position="left"
+          liveLink="https://aaronkg1.github.io/social-media-clone/"
+          repository="https://github.com/aaronkg1/social-media-clone"
+        />
+        <ProjectCard
+          title="Weather App"
+          description="Weather App built with vanilla JS."
+          url={weather}
+          position="right"
+          liveLink="https://aaronkg1.github.io/weather-app/"
+          repository="https://github.com/aaronkg1/weather-app"
+        />
+        <ProjectCard
+          title="Where's Wally App"
+          description="Where's Wally game with leaderboard built using React and Firebase."
+          url={wheresWally}
+          position="left"
+          liveLink="https://aaronkg1.github.io/where-is-wally/"
+          repository="https://github.com/aaronkg1/where-is-wally"
+        />
+        <ProjectCard
+          title="CV Creator App"
+          description="CV Creator App made with React."
+          url={cvcreator}
+          position="right"
+          liveLink="https://aaronkg1.github.io/cv-project/"
+          repository="https://github.com/aaronkg1/cv-project"
+        />
+        <ProjectCard
+          title="Battleships "
+          description="One player battleships game with smart AI opponent."
+          url={battleships}
+          position="left"
+          liveLink="https://aaronkg1.github.io/battleship/"
+          repository="https://github.com/aaronkg1/battleship"
+        />
+        <ProjectCard
+          title="Etch-a-sketch App"
+          description="Etch-a-sketch app created with vanilla JS."
+          url={etchasketch}
+          position="right"
+          liveLink="https://aaronkg1.github.io/etch-a-sketch/"
+          repository="https://github.com/aaronkg1/etch-a-sketch"
+        />
       </ul>
     </section>
   );
