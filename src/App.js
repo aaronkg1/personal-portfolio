de8982ad853bf81557eaa9867bd11f31a2a483f1
aaronkg1 <aaronkg1@gmail.com>
2022-08-01 11:00:47 +0100

    feat: Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import "./styles/App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Nav from "./components/Nav";
+import Spinner from "./components/SpinningMenu";
+import { useSelector } from "react-redux";
 function App() {
+  const theme = useSelector((state) => state.theme.theme);
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter hashType="noslash">
+      <div
+        className={theme === "dark" ? "page-container" : "page-container light"}
+      >
+        <Spinner />
+        <main className="main-content">
+          <Home />
+          <About />
+          <Projects />
+        </main>
+      </div>
+    </HashRouter>
   );
 }
 
