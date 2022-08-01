b2a987c26ed54189e966a0427cc58e3b2d280c8a
aaronkg1 <aaronkg1@gmail.com>
2022-08-01 11:03:20 +0100

    feat: Created home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
-import Spinner from "./SpinningMenu";
-
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+import "../styles/Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { HashLink } from "react-router-hash-link";
 const Home = () => {
   return (
-    <div className="page-container">
-      <Spinner />
-    </div>
+    <section className="welcome-container" id="welcome-message">
+      <div className="message-container">
+        <p>
+          Hi, I'm <span className="emphasise">Aaron</span>.
+        </p>
+        <p>A Full-Stack Web Developer.</p>
+        <HashLink smooth to="#about">
+          <button className="learn-more">
+            Learn More{" "}
+            <FontAwesomeIcon icon={faArrowDown} className="down-arrow" />
+          </button>
+        </HashLink>
+      </div>
+    </section>
   );
 };
 
